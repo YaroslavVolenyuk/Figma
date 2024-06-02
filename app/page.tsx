8fc88d4af2a00ed6adea3b5c99468a74f4aaa65c
Yaroslav Volenyuk <yaroslavvolenyuk@Yaroslavs-Air.fritz.box>
2024-06-02 20:00:25 +0200

    add second user cursor live

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ import { Room } from './Room';
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Figma</h1>
+    <div>
       <Live />
     </div>
   );
--- components/Live.tsx
+++ components/Live.tsx
@@ -1,11 +1,38 @@
-import { useOthers } from '@/liveblocks.config';
-import React from 'react';
+import { useMyPresence, useOthers } from '@/liveblocks.config';
+import React, { useCallback } from 'react';
 import LiveCursors from './cursor/LiveCursors';
 
 const Live = () => {
   const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center "
+    >
+      <h1 className="text-2xl text-white">Figma</h1>
       <LiveCursors others={others} />
     </div>
   );
