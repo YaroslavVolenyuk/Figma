e590c654b9c7218519a6fe9b5da85201d85dcc56
Yaroslav Volenyuk <yaroslavvolenyuk@Yaroslavs-Air.fritz.box>
2024-06-20 20:35:26 +0200

    Add geometry shapes to the canvas | navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,64 @@
 'use client';
 
+import LeftSideBar from '@/components/LeftSideBar';
 import Live from '@/components/Live';
+import RightSideBar from '@/components/RightSideBar';
+import Navbar from '@/components/users/Navbar';
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from '@/lib/canvas';
+import { ActiveElement, CustomFabricObject } from '@/types/type';
+import { fabric } from 'fabric';
 import Image from 'next/image';
+import { act, useEffect, useRef, useState } from 'react';
 import { Room } from './Room';
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Canvas | null>(null);
+  const selectedShapeRef = useRef<string | null>('rectangle');
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon: '',
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string;
+  };
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+    canvas.on('mouse:down', (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+
+    window.addEventListener('resize', () => {
+      handleResize({ fabricRef });
+    });
+  }, []);
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
+      <section className="flex h-full flex-row">
+        <LeftSideBar />
+        <Live canvasRef={canvasRef} />
+        <RightSideBar />
+      </section>
+    </main>
   );
 }
---
+++ components/RightSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideBar = () => {
+  return (
+    <section className="flex flex-col border-t border-x-primary-grey-300 min-2-[227px] sticky bg-primary-black left-0 h-full max-sm:hidden select-none overflow-y-auto">
+      <h3 className="px-5 pt-4 text-xs uppercase text-white">Design</h3>
+    </section>
+  );
+};
+
+export default RightSideBar;
